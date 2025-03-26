33db14ae8e65ee31f16ddc8c210ec1df9f403a45
lzamranoa <lzambranoa.software@gmail.com>
2025-03-25 21:09:30 -0500

    Creación del navbar y sidebar basicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
